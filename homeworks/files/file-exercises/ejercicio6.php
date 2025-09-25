96e4fd0fa6b1de623b2465197e062b6103ce162e
mackstm <thelewyntm@gmail.com>
2025-09-25 15:59:40 +0100

    exercise6 done

--- homeworks/files/file-exercises/ejercicio6.php
+++ homeworks/files/file-exercises/ejercicio6.php
@@ -1,3 +1,12 @@
 <?php
-    
+    /**
+     * @author eduglezexp
+     * @version 1.0.0
+     */
+
+    file_put_contents("resources/frase.txt", "Hola PHP");
+    $texto = file_get_contents("resources/frase.txt");
+    $textoInvertido = strrev($texto);
+    file_put_contents("resources/frase_invertida.txt", $textoInvertido);
+    echo file_get_contents("resources/frase_invertida.txt");
 ?>
