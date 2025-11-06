e82ae1236da7cb079b120fc6e609e0302fb979dc
eduglezexp <eduex04@gmail.com>
2025-11-06 16:33:58 +0000

    adding comments

--- homeworks/hotel-puerto/src/main/java/org/docencia/hotel/repository/interfaces/ICrudRepository.java
+++ homeworks/hotel-puerto/src/main/java/org/docencia/hotel/repository/interfaces/ICrudRepository.java
@@ -9,9 +9,38 @@ import java.util.List;
  */
 
 public interface ICrudRepository<T> {
+
+    /**
+     * Metodo para ver si existe la entity dada su id
+     * @param id de la entity
+     * @return true/false
+     */
     boolean existsById(String id);
+
+    /**
+     * Metodo para encontrar una entity dada su id
+     * @param id de la entity
+     * @return true/false
+     */
     T findById(String id);
+
+    /**
+     * Metodo para encontrar todas las entities
+     * @return una lista de entities
+     */
     List<T> findAll();
+
+    /**
+     * Metodo para guardar una entity
+     * @param entity a guardar
+     * @return entity a guardar
+     */
     T save(T entity);
+
+    /**
+     * Metodo para eliminar una entity dada su id
+     * @param id de la entity
+     * @return true/false
+     */
     boolean deleteById(String id);
 }
